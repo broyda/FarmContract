0a78a09a49dc7d916e07d055b51ed6746d1a8f0f
harisonde <harisonde@yahoo.co.in>
2018-04-14 02:51:37 -0500

    Added create, bid, acceptContract and transfer contract methods.

--- contracts/FarmContract.sol
+++ contracts/FarmContract.sol
@@ -1,26 +1,72 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
-contract FarmContract {
-    address public initiator;
-    
-    // Array containing the Domain struct (say the Farm agreement instances)
-    /*
-    struct FarmAgreement {
-        address public holder; // The insured or in our world the 'trader'
-        string latitude; // The latitude (say, the mean approx. latitude)
-        string longitude; // The latitude (say, the mean approx. latitude)
-        string meanElevationOverSeaLevel; // Perhaps required by the weather apis
-        string value; // See if using premium makes more sense
-        // An inner struct covering the perils (a la carte??)
-        
-        
-    }
-    
-    
-    */
+contract FarmContract{
+    address public owner;
+    string  latitude; // The latitude (say, the mean approx. latitude)
+    string longitude; // The latitude (say, the mean approx. latitude)
+    uint  coverageAmount; // Coverage required for this contract.
+    uint  listedPrice; // Amount ready to pay by creator/owner for contract to be covered
+    address public insurer; // Insurer for this contract
+    bool contractedByInsurer; //Bool ind to know whether contract is insured by insurer.
 
-    public FarmInsurance() public {
-        // The global msg object
-        initiator = msg.sender;
+    mapping(address => uint) public listOfBidders;//List of bidder with bidding price
+    uint public numberOfBidders; //Total number of bidders on this contract
+
+    function FarmContract(string _latitude, string _longitude,
+    uint _coverageAmount,  uint _listedPrice) public {
+        owner = msg.sender;
+        latitude = _latitude;
+        longitude = _longitude;
+        coverageAmount = _coverageAmount;
+        listedPrice = _listedPrice;
     }
+
+    //Tobe invoked by insurers with bidding amount for the contract
+    function bid(uint amount) biddingRules(amount) checkInsurer public {
+        if(listOfBidders[msg.sender] == 0){
+             numberOfBidders++;
+        }
+        listOfBidders[msg.sender] = amount;
+    }
+
+    //to be invoked by owner to select list of contracts
+    function chooseBidder(address _bidderAddress) onlyOwner checkInsurer public payable{
+        require(listOfBidders[_bidderAddress] != 0);
+        require(msg.value >= listOfBidders[_bidderAddress]);
+        insurer = _bidderAddress;
+    }
+
+    //to be invoked by Insurer to accept the contract
+    function acceptContract() public payable{
+        require(contractedByInsurer == false);
+        require(msg.sender == insurer);
+        require(msg.value >= coverageAmount);
+        insurer.transfer(listOfBidders[msg.sender]);
+        contractedByInsurer = true;
+    }
+
+    function contractBalance() view public returns(uint){
+        return this.balance;
+    }
+
+    function transferContract(address newOwner) onlyOwner public{
+        owner = newOwner;
+    }
+
+    modifier biddingRules(uint bidPrice){
+        require(bidPrice > 0);
+        require(insurer == 0);
+        _;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier checkInsurer(){
+        require(insurer == 0);
+        _;
+    }
+
 }
