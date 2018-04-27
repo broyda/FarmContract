b826a8c51f27a75357d5e751be02c3c0a9f14aa2
harisonde <harisonde@yahoo.co.in>
2018-04-26 19:50:36 -0500

    Installed Next.js, react and ReactDom. And added index.js page.

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'D:\\workspace\\FarmContract\\pages\\index.js?entry';
+
+exports.default = function () {
+  return _react2.default.createElement('div', { style: { marginTop: '25px' }, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement('hr', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }), _react2.default.createElement('h3', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, _react2.default.createElement('center', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, 'Welcome to Farm Contract!!!')), _react2.default.createElement('hr', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    }
+  }));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkNvbXBvbmVudCIsIm1hcmdpblRvcCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUFRLEFBRWY7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDbkI7eUJBQ0UsY0FBQSxTQUFLLE9BQU8sRUFBQyxXQUFiLEFBQVksQUFBVztnQkFBdkI7a0JBQUEsQUFDQTtBQURBO0dBQUE7O2dCQUNBO2tCQURBLEFBQ0EsQUFDQTtBQURBO0FBQUEsc0JBQ0EsY0FBQTs7Z0JBQUE7a0JBQUEsQUFBSTtBQUFKO0FBQUEscUJBQUksY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBRkosQUFFQSxBQUFJLEFBQ0o7O2dCQUFBO2tCQUpGLEFBQ0UsQUFHQSxBQUdIO0FBSEc7QUFBQTtBQUxKIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkQ6L3dvcmtzcGFjZS9GYXJtQ29udHJhY3QifQ==
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default () => {
+  return(
+    <div style={{marginTop:'25px'}}>
+    <hr/>
+    <h3><center>Welcome to Farm Contract!!!</center></h3>
+    <hr/>
+    </div>
+  );
+}
