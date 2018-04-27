cc03ffc9a8e45e7652684ff0e54361ebe4761742
harisonde <harisonde@yahoo.co.in>
2018-04-26 20:27:34 -0500

    Added a RandomNumberGenerator api to generate Random number by making an API to call to outside world using Oraclize service.

---
+++ contracts/RandomNumberOracle.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+import ""./OraclizeApi.sol";
+
+
+contract RandomNumberOracle is usingOraclize {
+
+  string public numberGenerated;
+  bytes32 public oraclizeID;
+
+  function generateRandamNumber() payable public {
+    oraclizeID = oraclize_query("WolframAlpha", "random number");
+  }
+
+  function __callback(bytes32 _oraclizeID, string _result) public {
+    if(msg.sender != oraclize_cbAddress()) throw;
+    numberGenerated = _result;
+  }
+}
